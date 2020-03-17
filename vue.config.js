bd62a0f47b7a95dd5a3901ce33112f98fd6a097c
何金峰 <kingphone_he@163.com>
2020-03-17 18:39:52 +0800

    1：添加favicon； 2：修改build配置；

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? "../" : "/",
+    productionSourceMap: false,
+    // outputDir: 'dist',
+    // assetsDir: 'static',
+    // indexPath: 'index.html'
+};
+
